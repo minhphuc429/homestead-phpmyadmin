a151d47c50e3ec9685bf84adc4d6185a7d4241d6
Phuc Nguyen Minh <minhphuc429@gmail.com>
2017-11-13 19:38:52 +0700

    create certificates upon installation

--- phpmyadmin.sh
+++ phpmyadmin.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 echo 'Downloading phpMyAdmin 4.7.5'
-curl -#L https://files.phpmyadmin.net/phpMyAdmin/4.7.5/phpMyAdmin-4.7.5-all-languages.tar.gz -o phpmyadmin.tar.gz
+curl -#L https://files.phpmyadmin.net/phpMyAdmin/4.7.5/phpMyAdmin-4.7.5-english.tar.gz -o phpmyadmin.tar.gz
 
 mkdir phpmyadmin && tar xf phpmyadmin.tar.gz -C phpmyadmin --strip-components 1
 
 rm phpmyadmin.tar.gz
 
 CMD=/vagrant/scripts/serve-laravel.sh
+CMD_CERT=/vagrant/scripts/create-certificate.sh
+
 if [ ! -f $CMD ]; then
-    # fallback for older versions
+    # Fallback for older Homestead versions
     CMD=/vagrant/scripts/serve.sh
+else
+    # Create an SSL certificate
+    sudo bash $CMD_CERT phpmyadmin.app
 fi
 
 sudo bash $CMD phpmyadmin.app $(pwd)/phpmyadmin
